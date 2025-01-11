1c9d176b85c8abf97c162f046b1487eb391926b6
Diego Sousa <me@diego.dev.br>
2025-01-11 00:21:30 -0300

    Post Anime

--- anime-service/src/main/java/br/dev/diego/animeservice/controller/AnimeController.java
+++ anime-service/src/main/java/br/dev/diego/animeservice/controller/AnimeController.java
@@ -1,19 +1,24 @@
 package br.dev.diego.animeservice.controller;
 
 import br.dev.diego.animeservice.domain.Anime;
+import br.dev.diego.animeservice.domain.AnimeRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("v1/animes")
 public class AnimeController {
 
-    private final List<Anime> animes = new Anime().obterAnimesList();
+    private final List<Anime> animes = new ArrayList<>(new Anime().obterAnimesList());
 
     @GetMapping
     public List<Anime> obterAnimes(@RequestParam(required = false) String nome) {
@@ -29,6 +34,14 @@ public class AnimeController {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow();
+    }
+
+    @PostMapping
+    public Anime salvarAnime(@RequestBody AnimeRequest anime) {
+        Long id = new Random().nextLong(999);
+        Anime animeSaved = new Anime(id, anime.getName());
+        animes.addLast(animeSaved);
+        return animeSaved;
     }
 
 }
---
+++ anime-service/src/main/java/br/dev/diego/animeservice/domain/AnimeRequest.java
@@ -0,0 +1,22 @@
+package br.dev.diego.animeservice.domain;
+
+public class AnimeRequest {
+
+    private String name;
+
+    public AnimeRequest() {
+    }
+
+    public AnimeRequest(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
