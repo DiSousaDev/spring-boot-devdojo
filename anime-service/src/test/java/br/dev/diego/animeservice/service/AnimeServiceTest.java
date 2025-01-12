135d7c0d9b9c69030ff5aa59c22a9dd3181a1d8d
Diego Sousa <me@diego.dev.br>
2025-01-12 17:09:22 -0300

    Adicionando testes nos Services

---
+++ anime-service/src/test/java/br/dev/diego/animeservice/service/AnimeServiceTest.java
@@ -0,0 +1,142 @@
+package br.dev.diego.animeservice.service;
+
+import br.dev.diego.animeservice.domain.Anime;
+import br.dev.diego.animeservice.repository.AnimeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class AnimeServiceTest {
+
+    @InjectMocks
+    private AnimeService service;
+
+    @Mock
+    private AnimeRepository repository;
+
+    private List<Anime> animeList = new ArrayList<>();
+
+    private Anime anime1;
+
+    @BeforeEach
+    void setUp() {
+        anime1 = new Anime(1L, "Naruto Test");
+        Anime anime2 = new Anime(2L, "One Piece Test");
+        Anime anime3 = new Anime(3L, "Attack on Titan Test");
+        Anime anime4 = new Anime(4L, "My Hero Academia Test");
+        animeList.addAll(List.of(anime1, anime2, anime3, anime4));
+    }
+
+    @Test
+    @DisplayName("Deve retornar todos animes quando o nome estiver nulo")
+    @Order(1)
+    void findAll_deve_buscar_todos_animes() {
+        when(repository.findAll()).thenReturn(animeList);
+        List<Anime> animes = service.buscar(null);
+        assertFalse(animes.isEmpty());
+        assertEquals(4, animes.size());
+        assertThat(animes).hasSameElementsAs(animeList);
+    }
+
+    @Test
+    @DisplayName("Deve retornar apenas 1 anime quando o nome é encontrado")
+    @Order(2)
+    void findAll_deve_buscar_um_anime() {
+        when(repository.findAll()).thenReturn(animeList);
+        when(repository.findByName("Toei Animation")).thenReturn(List.of(anime1));
+        List<Anime> animes = service.buscar("Toei Animation");
+        assertFalse(animes.isEmpty());
+        assertEquals(1, animes.size());
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma lista vazia quando o nome não é encontrado")
+    @Order(3)
+    void findAll_deve_retornar_lista_vazia() {
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+        List<Anime> animes = service.buscar(null);
+        assertTrue(animes.isEmpty());
+        verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("Deve salvar um anime")
+    @Order(4)
+    void save_deve_salvar_um_anime() {
+        Anime anime = new Anime(6L, "New Anime");
+        when(repository.save(anime)).thenReturn(anime);
+        Anime savedAnime = service.save(anime);
+        assertEquals(anime, savedAnime);
+        verify(repository, times(1)).save(anime);
+    }
+
+    @Test
+    @DisplayName("Deve buscar um anime por ID")
+    @Order(5)
+    void buscarPorId_deve_retornar_um_anime() {
+        Anime anime = new Anime(6L, "New Anime");
+        when(repository.findById(1L)).thenReturn(anime);
+        Anime foundAnime = service.buscarPorId(1L);
+        assertEquals(anime, foundAnime);
+        verify(repository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("Deve atualizar um anime")
+    @Order(6)
+    void atualizar_deve_atualizar_um_anime() {
+        Anime anime = new Anime(6L, "New Anime");
+        when(repository.findById(1L)).thenReturn(anime);
+        Anime updatedAnime = service.atualizar(1L);
+        assertEquals(anime, updatedAnime);
+        verify(repository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("Deve deletar um anime por ID")
+    @Order(7)
+    void deletar_deve_deletar_um_anime() {
+        doNothing().when(repository).deleteById(1L);
+        service.deletar(1L);
+        verify(repository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    @DisplayName("Deve lancar uma excecao quando nao encontra o anime")
+    @Order(8)
+    void deletar_deve_lancar_ResponseStatusException() {
+        doThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found")).when(repository).deleteById(1L);
+
+        ResponseStatusException exception = org.junit.jupiter.api.Assertions.assertThrows(ResponseStatusException.class, () -> {
+            service.deletar(1L);
+        });
+
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+    }
+
+}
--- anime-service/src/test/java/br/dev/diego/animeservice/service/ProducerServiceTest.java
+++ anime-service/src/test/java/br/dev/diego/animeservice/service/ProducerServiceTest.java
@@ -12,11 +12,22 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -31,10 +42,15 @@ class ProducerServiceTest {
 
     private List<Producer> producerList = new ArrayList<>();
 
+    private Producer producer1;
+
     @BeforeEach
     void setUp() {
+
+        producer1 = new Producer(1L, "Toei Animation", LocalDateTime.now().minusDays(2));
+
         producerList.addAll(List.of(
-                new Producer(1L, "Toei Animation", LocalDateTime.now().minusDays(2)),
+                producer1,
                 new Producer(2L, "Madhouse", LocalDateTime.now().minusDays(3)),
                 new Producer(3L, "Bones", LocalDateTime.now().minusDays(4)),
                 new Producer(4L, "Sunrise", LocalDateTime.now().minusDays(5)),
@@ -43,12 +59,90 @@ class ProducerServiceTest {
     }
 
     @Test
-    @DisplayName("Deve buscar todos producers quando o nome estiver nulo")
+    @DisplayName("Deve retornar todos producers quando o nome estiver nulo")
     @Order(1)
     void findAll_deve_buscar_todos_producers() {
         when(repository.findAll()).thenReturn(producerList);
-//        List<ProducerResponse> producers = service.buscar(null);
-//        assertFalse(producers.isEmpty());
-//        assertEquals(4, producers.size());
+        List<Producer> producers = service.buscar(null);
+        assertFalse(producers.isEmpty());
+        assertEquals(5, producers.size());
+        assertThat(producers).hasSameElementsAs(producerList);
     }
+
+    @Test
+    @DisplayName("Deve retornar apenas 1 producer quando o nome é encontrado")
+    @Order(2)
+    void findAll_deve_buscar_um_producer() {
+        when(repository.findAll()).thenReturn(producerList);
+        when(repository.findByName("Toei Animation")).thenReturn(List.of(producer1));
+        List<Producer> producers = service.buscar("Toei Animation");
+        assertFalse(producers.isEmpty());
+        assertEquals(1, producers.size());
+    }
+
+    @Test
+    @DisplayName("Deve retornar uma lista vazia quando o nome não é encontrado")
+    @Order(3)
+    void findAll_deve_retornar_lista_vazia() {
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+        List<Producer> producers = service.buscar(null);
+        assertTrue(producers.isEmpty());
+        verify(repository, times(1)).findAll();
+    }
+
+    @Test
+    @DisplayName("Deve salvar um producer")
+    @Order(4)
+    void save_deve_salvar_um_producer() {
+        Producer producer = new Producer(6L, "New Producer", LocalDateTime.now());
+        when(repository.save(producer)).thenReturn(producer);
+        Producer savedProducer = service.save(producer);
+        assertEquals(producer, savedProducer);
+        verify(repository, times(1)).save(producer);
+    }
+
+    @Test
+    @DisplayName("Deve buscar um producer por ID")
+    @Order(5)
+    void buscarPorId_deve_retornar_um_producer() {
+        Producer producer = new Producer(6L, "New Producer", LocalDateTime.now());
+        when(repository.findById(1L)).thenReturn(producer);
+        Producer foundProducer = service.buscarPorId(1L);
+        assertEquals(producer, foundProducer);
+        verify(repository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("Deve atualizar um producer")
+    @Order(6)
+    void atualizar_deve_atualizar_um_producer() {
+        Producer producer = new Producer(6L, "New Producer", LocalDateTime.now());
+        when(repository.findById(1L)).thenReturn(producer);
+        Producer updatedProducer = service.atualizar(1L);
+        assertEquals(producer, updatedProducer);
+        verify(repository, times(1)).findById(1L);
+    }
+
+    @Test
+    @DisplayName("Deve deletar um producer por ID")
+    @Order(7)
+    void deletar_deve_deletar_um_producer() {
+        doNothing().when(repository).deleteById(1L);
+        service.deletar(1L);
+        verify(repository, times(1)).deleteById(1L);
+    }
+
+    @Test
+    @DisplayName("Deve lancar uma excecao quando nao encontra o producer")
+    @Order(8)
+    void deletar_deve_lancar_ResponseStatusException() {
+        doThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Producer not found")).when(repository).deleteById(1L);
+
+        ResponseStatusException exception = org.junit.jupiter.api.Assertions.assertThrows(ResponseStatusException.class, () -> {
+            service.deletar(1L);
+        });
+
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
+    }
+
 }
